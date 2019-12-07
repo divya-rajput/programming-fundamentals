1c19b6e52cc24cbef140f4091e3ce6f117fc91c7
Divya Rajput <divya.rajput8220@gmail.com>
2019-12-07 18:36:27 +0530

    Added code for fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,56 @@
+# For any given value n, find the nth fibonacci value.
+# Position: 1 2 3 4 5 6 7 8  9  10 ...
+# Value   : 0 1 1 2 3 5 8 13 21 34 ...
+
+# Recusrive solution
+# TC: O(2^n)
+def fibo_recur(n):
+    if n<=0:
+        return -1
+    if n==1 or n==2:
+        return n-1
+    return fibo_recur(n-2)+ fibo_recur(n-1)
+
+# Iteartive solution using array
+# TC: O(n), ASC: O(n)
+def fibo_arr(n):
+    if n<=0:
+        return -1
+    arr=[0,1]
+    for x in range(n-2):
+        arr.append(arr[-2]+arr[-1])
+    # print(arr)
+    return arr[n-1]
+
+# Iteartive solution
+# TC: O(n), ASC: O(1)
+def optimized_fibo(n):
+    if n<=0:
+        return -1
+    if n==1 or n==2:
+        return n-1
+    # s1=0
+    # s2=1
+    # s3=-1
+    s1, s2, s3 = 0, 1, -1
+    for x in range(n-2):
+        s3=s2+s1
+        s1=s2
+        s2=s3
+    return s3
+
+# Test cases
+n=10
+expected = 34
+
+actual = fibo_recur(n)
+assert expected == actual, "Testcase failed: Expected %d, got %d"%(expected, actual)
+print("Test Case passed for Recursive Solution")
+
+actual = fibo_arr(n)
+assert expected == actual, "Testcase failed: Expected %d, got %d"%(expected, actual)
+print("Test Case passed for Iterative Solution with Array")
+
+actual = optimized_fibo(n)
+assert expected == actual, "Testcase failed: Expected %d, got %d"%(expected, actual)
+print("Test Case passed for Iterative Solution")
